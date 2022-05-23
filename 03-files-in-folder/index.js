3b2d5fc07ee436249e5caa77add6eaa8018b547a
svoboda88 <zhenya.dowgun@gmail.com>
2022-05-23 20:38:57 +0300

    feat: done tasks 3, 4

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,2 +1,23 @@
 const fs = require('fs');
 const path = require('path');
+
+const dirPath = path.join(__dirname, 'secret-folder');
+
+const getFileInfo = (fileName) => {
+  fs.stat(`${dirPath}/${fileName}`, (err, stats) => {
+    if(err) throw err;
+    if (stats.isFile()) {
+      const result = fileName.match(/(.+?)(\.[^.]*$|$)/);
+
+      console.log(`${result[1]} - ${result[2]}`+' - ' + stats['size'] + ' bytes');
+    }
+  
+  });
+};
+
+fs.readdir(dirPath, (err, files) => {
+  files.forEach((file) => {
+    getFileInfo(file);
+  });
+});
+
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,45 @@
+const Path = require('path');
+const FSP = require('fs').promises;
+const fs = require('fs');
+
+const srcFilename  = __dirname + '/files';
+const destFilename  = __dirname + '/files-copy';
+
+const dirExists = (file) => {
+  return new Promise((resolve) => {
+    fs.access(file, fs.constants.F_OK, (err) => {
+      err ? resolve(false) : resolve(true);
+    });
+  });
+};
+
+async function createCopyDir(src,dest) {
+  const entries = await FSP.readdir(src, {withFileTypes: true});
+  await FSP.mkdir(dest);
+  for(let entry of entries) {
+    const srcPath = Path.join(src, entry.name);
+    const destPath = Path.join(dest, entry.name);
+    if(entry.isDirectory()) {
+      await createCopyDir(srcPath, destPath);
+    } else {
+      await FSP.copyFile(srcPath, destPath);
+    }
+  }
+}
+
+async function copyDir () {
+  const isDirExists = await dirExists(destFilename);
+
+  if(isDirExists) {
+    fs.rm(destFilename, { recursive: true },  () => {
+      createCopyDir(srcFilename, destFilename);
+    });
+
+  } else {
+    createCopyDir();
+  }
+
+}
+
+copyDir();
 
